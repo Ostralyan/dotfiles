4407146b1a75e0b6491890eaa5b92b34227c73c3
Luke Xu <lx@didero.ai>
2026-03-06 23:00:45 -0500

    chore(brew): add karabiner-elements to cask install list

--- private_dot_config/private_fish/functions/setup_brew_packages.fish
+++ private_dot_config/private_fish/functions/setup_brew_packages.fish
@@ -1,6 +1,6 @@
 function setup_brew_packages -d "Installs all my used brew packages"
   brew install atuin bat eza fd fisher fzf go neovim pgcli ripgrep rustup starship tmux zoxide chezmoi awscli uv 
-  brew install --cask alt-tab claude claude-code ghostty spotify claude gcloud-cli itsycal rectangle font-fantasque-sans-mono-nerd-font iina figma thebrowsercompany-dia sidebar slack session-manager-plugin raycast orbstack notion linear-linear homerow aerospace
+  brew install --cask alt-tab claude claude-code ghostty spotify claude gcloud-cli itsycal rectangle font-fantasque-sans-mono-nerd-font iina figma thebrowsercompany-dia sidebar slack session-manager-plugin raycast orbstack notion linear-linear homerow aerospace karabiner-elements
 
   bat cache --build
 end
